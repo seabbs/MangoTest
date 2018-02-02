671807e648aa54baba3daab1546b8f4316eb68a9
seabbs <s.e.abbott12@gmail.com>
2018-02-02 13:29:20 +0000

    added test function

--- tests/testthat/test-summary.sample_dist.R
+++ tests/testthat/test-summary.sample_dist.R
@@ -8,9 +8,10 @@ test <- sample_dist(1000, dist = rnorm)
 result <- summary(test)
 
 test_summary_measure <- function(test, result, sum_fn, ...) {
-  test_that(paste0("summary.sample_dist calculates the",  sum_fn,  " correctly"), {
-    expect_equal(result, sum_fn(test, ...))
+  test_that(paste0("summary.sample_dist calculates the ",   as.character(quote(sum_fn)),  " correctly"), {
+    expect_equal(as.numeric(result), sum_fn(test, ...))
   })
 }
 
-test_summary_measure(test, result$Mean, sum_fn = mean)
+test_summary_measure(test, result["Mean"], sum_fn = mean)
+test_summary_measure(test, result["Median"], sum_fn = median)
